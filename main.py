077877153b51acc4e411f178d964569251781748
Andrii Olkhovych <andrii.olkhovych@pm.me>
2025-06-01 21:22:58 +0200

    Place jammer and drone randomly

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from config import MAP_SIZE
 from drone import Drone
 from jammer import Jammer
 
 
 def main():
-    d = Drone((50, 50))
-    j = Jammer((20, 60))
+    pos_d = np.random.randint(low=0, high=MAP_SIZE, size=2)
+    pos_j = np.random.randint(low=0, high=MAP_SIZE, size=2)
+
+    d = Drone(tuple(pos_d))
+    j = Jammer(tuple(pos_j))
+
     plt.plot(d.pos[0], d.pos[1], "bo")
     plt.plot(j.pos[0], j.pos[1], "ro")
     plt.xlim([0, MAP_SIZE])
